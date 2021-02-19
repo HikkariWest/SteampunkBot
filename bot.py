6e681b7354d50abaa5976718886ab853b548b9c8
Muradil <04072002mu@gmail.com>
2021-02-19 22:55:09 +0600

    fix

--- bot.py
+++ bot.py
@@ -1,5 +1,4 @@
 import time
-import schedule
 import telebot
 from requests import get
 from bs4 import BeautifulSoup
@@ -15,36 +14,31 @@ bot = telebot.TeleBot(API_TOKEN, parse_mode=None)
 url = get('https://www.playground.ru/news').content
 soup = BeautifulSoup(url, 'html.parser')
 div = soup.find_all("div", {"class": "post-title"})
-a = []
+links = []
 
 div_img = soup.find_all("article", {"class": "post"})
-b = []
 
 posts = []
-post = {}
 
-for lnk in div_img:
-    b.append(lnk.find('img')['src'])
-
-
-for j in b:
-    name_img = j.split('/')[-1]
-    name = name_img[:-8]
-    post['img'] = name
 
 
 for link in div:
-    a.append(link.find('a')['href'])
+    links.append(link.find('a')['href'])
 
-for i in range(int(len(a))):
-    url = get(a[i]).content
-    soup = BeautifulSoup(url, 'html.parser')
+for i in range(int(len(links))):
+    post = {}
+    j = div_img[i].find('img')['src']
+    name_img = j.split('/')[-1]
+    name = name_img[:-8]
+    post['img'] = name
+    url = links[i]
+    page = get(links[i]).content
+    soup = BeautifulSoup(page, 'html.parser')
     h1 = soup.h1.text
     p = soup.p.text
     post['h1'] = h1
     post['p'] = p
-
-posts.append(post)
+    posts.append(post)
 
 
 
@@ -54,25 +48,21 @@ def send_welcome(message):
     item1 = types.KeyboardButton("OK")
     item2 = types.KeyboardButton("STOP")
     markup.add(item1, item2)
-    bot.send_message(message.chat.id, 'hello, {0.first_name}!\nЯ игровой новостной паблик! \n Буду постить актуальные новости!'.format(message.from_user, bot.get_me(), parse_mode='html'), reply_markup=markup)
+    bot.send_message(message.chat.id, 'Hello, {0.first_name}!\nЯ игровой новостной паблик! \nБуду постить актуальные новости!'.format(message.from_user, bot.get_me(), parse_mode='html'), reply_markup=markup)
 
 
 @bot.message_handler(content_types=['text'])
 def answer(message):
-    def send_post():
-        for post in posts:
-            bot.send_message(message.chat.id, 'https://i.playground.ru/e/' + post['img']  + "\n" + post['h1']+"\n \n"+post['p'])
 
-    schedule.every(1).minutes.do(send_post)
     if message.chat.type == "private":
         while True:
             if message.text == 'OK':
-                schedule.run_pending()
-                time.sleep(1)
+                for post in posts:
+                    bot.send_message(message.chat.id, post['h1']+"\n \n"+post['p'] + '\n https://i.playground.ru/e/' + post['img'])
+                time.sleep(1 * 60)
             else:
                 bot.send_message(message.chat.id, "Good bye")
-                schedule.clear()
-                time.sleep(1)
+                time.sleep(1 * 60)
                 break
 
 bot.polling()
